1656bae516a35107d1ac7872f9e41d45c5fd53bb
albertazakhi <albertazakhi11793@gmail.com>
2023-03-23 15:40:33 +0700

    perbaruan

--- js/script.js
+++ js/script.js
@@ -16,14 +16,12 @@ document.addEventListener('click', function (e) {
 
 const scriptURL = 'https://script.google.com/macros/s/AKfycbwp0xHuen4_If4mLXnjtlZHU9mLJil1uMYSne1eHloxx1BuV7R8Xl-s9eYvHE32HqWGzA/exec';
 const form = document.forms['brn-contact-form'];
-const myAlert = document.querySelector('.my-alert');
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   // Ketika tombol kirim di klik
   fetch(scriptURL, { method: 'POST', body: new FormData(form) })
     .then((response) => {
-      myAlert.classList.toggle('display-none');
       form.reset();
       console.log('Success!', response);
     })
